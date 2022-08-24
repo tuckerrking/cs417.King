eabd7e2dd11732abaad76932851c1b83c79772c5
Tucker King <tuckerrking@gmail.com>
2022-08-24 09:38:39 -0500

    Update assignment1_1.cpp
    Added a vertex structure and the adjacency list class

--- apps/apptemplate/source/assignment1_1.cpp
+++ apps/apptemplate/source/assignment1_1.cpp
@@ -6,7 +6,33 @@
 // adjacency matrix representations of a graph.
 //
 #include <iostream>
+#include <vector>
+#include <map>
+#include <string>
 using namespace std;
+
+struct vertex {
+  typedef pair<int, vertex*> ve;
+  vector<ve> adj;
+  string name;
+  vertex(string s) : name(s) {};
+};
+
+class graph_list {
+public:
+    typedef map<string, vertex *> vmap;
+    vmap work;
+    void addVertex(const string&);
+    void addEdge(const string& from, const string& to, double cost);
+    void addNode(ValueType x);
+    void deleteEdge(ValueType source, ValueType dest);
+    void deleteNode(ValueType node);
+    bool adjacent(ValueType x, ValueType y);
+    vector<ValueType> neighbors(ValueType x);
+    
+};
+
+
 
 int main() {
   // Put your code here.
