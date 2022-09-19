d286272b3c2a6d4bf24a8010f78c7c3608667d0f
Tucker King <tuckerrking@gmail.com>
2022-09-19 16:22:48 -0500

    Create assignment2_2.cpp

---
+++ apps/apptemplate/source/assignment2_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <fstream>
+#include <memory>
+#include <chrono>
+#include <random>
+
+int get_rand(int, int);
+
+int main()
+{
+  
+}
